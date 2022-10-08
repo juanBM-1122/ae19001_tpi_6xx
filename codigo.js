ecb12a061f00d61937752ca794f60ba6d2e85251
juanBM-1122 <100696845+juanBM-1122@users.noreply.github.com>
2022-10-08 08:00:09 -0600

    se modifico css y se agrego identificacion personal

--- codigo.js
+++ codigo.js
@@ -1,3 +1,6 @@
+/*Juan Carlos Acosta Escobar
+AE19001*/
+
 var fila="<tr><td class='id'></td><td class='foto'></td><td class='price'></td><td class='title'></td><td class='description'></td><td class='category'></td></tr>";
 	 var productos=null;
   function codigoCat(catstr) {
